a3d90092e49d7830df32188c516d7a036e6d0375
Master-PLC <18358716036@163.com>
2026-05-06 23:30:46 +0800

    'daily update'

--- ts_benchmark/utils/random_utils.py
+++ ts_benchmark/utils/random_utils.py
@@ -27,6 +27,8 @@ def fix_all_random_seed(seed: Optional[int] = 2021) -> NoReturn:
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.enabled = False
+
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     torch.use_deterministic_algorithms(True)
 
     os.environ["PYTHONHASHSEED"] = str(1)
